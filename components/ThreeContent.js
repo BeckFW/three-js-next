9a0deeeace9ae0daf462387c83b75fac3377ee05
Beck <beck@elbo.me>
2023-11-16 13:21:25 +0000

    Changes

---
+++ components/ThreeContent.js
@@ -0,0 +1,30 @@
+export default function ThreeContent() {
+    return(
+        <main>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+      <div class="groundSeperatorGroup">
+        <div class="groundSeperator">G</div>
+        <p>Content</p>
+        <div class="groundSeperator">M</div>
+        <p>Content</p>
+        <div class="groundSeperator">01</div>
+        <p>Content</p>
+        <div class="groundSeperator">02</div>
+        <p>Content</p>
+        <div class="groundSeperator">03</div>
+        <p>Content</p>
+        <div class="groundSeperator">04</div>
+        <p>Content</p>
+        <div class="groundSeperator">05</div>
+        <p>Content</p>
+        <div class="groundSeperator">06</div>
+        <p>Content</p>
+        <div class="groundSeperator">07</div>
+        <p>Content</p>
+        <div class="groundSeperator">08</div>
+      </div>
+        </main>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,45 @@
-const ClientSideAR = dynamic(
-    () => import('../components/clientAR'),
-     {
-       ssr: false
-     }
-    );
-  
-  const ClientSideThree = dynamic(
-    () => import('../components/threeAR'), 
-    {
-      ssr: false
-    }
-  
-  );
-  
-  function Home() {
-  
+import dynamic from 'next/dynamic';
+import { Environment, ContactShadows, OrbitControls, Html } from '@react-three/drei';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import ThreeContent from '../components/ThreeContent';
+
+import { useRef } from 'react';
+
+  function Model(props) {
+    const group = useRef();
+
+
   return (
-    <ClientSideThree />
-  )
+      <group ref={group} {...props} dispose={null}>
+        <group position={[0, 0, 0]} rotation={[0, 0.5, 0]}>
+          {/* Plane Mesh that HTML Content anchors too. Controls HTML position/rotation. */}
+          <mesh>
+            {/* Drei's HTML component can "hide behind" canvas geometry */}
+            <Html className="content" rotation-x={0} position={[0, 0, 0]} transform occlude>
+              <div className="wrapper" onPointerDown={(e) => e.stopPropagation()}>
+                <ThreeContent />
+              </div>
+            </Html>
+          </mesh>
+        </group>
+      </group>
+    )
+
+}
+
+
+  export default function Home() {
+      return (
+        <Canvas camera={{ position: [0, 0, 0], fov: 55 }}>
+        <pointLight position={[10, 10, 10]} intensity={1.5} />
+        <Suspense fallback={null}>
+          <group rotation={[0, 0, 0]} position={[0, 1, 0]}>
+            <Model />
+          </group>
+          <Environment preset="city" />
+        </Suspense>
+        <ContactShadows position={[0, 0, 0]} scale={20} blur={2} far={4.5} />
+      </Canvas>
+      )
   }
-  
-  export default Home
