404ff04d7a5e4388535adaffa92aaabb84b8cf04
Beck <beck@elbo.me>
2023-11-17 15:57:26 +0000

    Update ThreeContent.js to use array.map()

--- components/ThreeContent.js
+++ components/ThreeContent.js
@@ -1,30 +1,57 @@
 export default function ThreeContent() {
+
+  const floors = [
+    {
+      name: "G", 
+      fullName: "Ground",
+      content: "Entrance, Lobby, Reception",
+    }, 
+    {
+      name: "M",
+      fullName: "Mezzanine", 
+      content: "Cafe, Restaurant, Careers",
+    }, 
+    {
+      name: "01",
+      fullName: "First Floor", 
+      content: "Computer Rooms, Huxley Link",
+    },
+    {
+      name: "02",
+      fullName: "Second Floor", 
+      content: "Games Studios, Computer Rooms",
+    },
+    {
+      name: "03",
+      fullName: "Third Floor", 
+      content: "Computer Rooms",
+    },
+    {
+      name: "04",
+      fullName: "Fourth Floor", 
+      content: "Lab Rooms",
+    },
+    {
+      name: "05",
+      fullName: "Fifth Floor", 
+      content: "Offices",
+    },
+  ]   
+
     return(
-        <main>
-        <div>
-            <h1>Hello World</h1>
-        </div>
+      <main className="relative">
+
       <div class="groundSeperatorGroup">
-        <div class="groundSeperator">G</div>
-        <p>Content</p>
-        <div class="groundSeperator">M</div>
-        <p>Content</p>
-        <div class="groundSeperator">01</div>
-        <p>Content</p>
-        <div class="groundSeperator">02</div>
-        <p>Content</p>
-        <div class="groundSeperator">03</div>
-        <p>Content</p>
-        <div class="groundSeperator">04</div>
-        <p>Content</p>
-        <div class="groundSeperator">05</div>
-        <p>Content</p>
-        <div class="groundSeperator">06</div>
-        <p>Content</p>
-        <div class="groundSeperator">07</div>
-        <p>Content</p>
-        <div class="groundSeperator">08</div>
+      {floors.map((floor) => {
+        return(
+          <div className="w-full">
+            <div class="groundSeperator">{floor.name}</div>
+            <p>{floor.content}</p>
+        </div>
+        )
+      })}
       </div>
+      
         </main>
     )
 }
