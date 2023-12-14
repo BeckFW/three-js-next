c54a9b7935daaff1ad364acf9ff5754885c6358d
Beck <beck@elbo.me>
2023-12-14 12:42:30 +0000

    Remove unused comments and minimise rotation values

--- components/clientSideWebXR.js
+++ components/clientSideWebXR.js
@@ -10,14 +10,13 @@ function Model(props) {
   
   return (
     <group ref={group} {...props} dispose={null}>
-      <group position={[0, 0, 0]} rotation={[0, 0.3, 0]}>
-        {' '}
+      <group position={[0, 0, 0]} rotation={[0, 0, 0]}>
         {/* Plane Mesh that HTML Content anchors too. Controls HTML position/rotation. */}
         <mesh>
-          {/* Drei's HTML component can "hide behind" canvas geometry */}
-          <Html className="content" rotation-x={0} scale={50} position={[0, 0, 0.06 ]}  transform >
+          <Html className="content" rotation-x={0} scale={100} position={[0, 0, 0 ]}  transform >
             <div className="wrapper" onPointerDown={(e) => e.stopPropagation()}>
               <div className='relative w-full text-center flex'>
+                {/* HTML Content */}
               <ThreeContent/>
               </div>
             </div>
@@ -32,25 +31,23 @@ export default function ClientXR() {
 
   return (
     <>
-    {/*<div style={{zIndex: 1000, position: "relative"}}>*/}
     <ARButton />
- {/* </div>*/}
     <Canvas 
       style={{ position: 'absolute', top: 0, left: 0 }} 
-      camera={{ position: [-5, 0, -15], fov: 55, zoom: 5}}
+      camera={{ position: [-5, 0, -15], fov: 55, zoom: 50}}
       >
       <XR referenceSpace='local'>
       <pointLight position={[10, 10, 10]} intensity={1.5} />
       <Suspense fallback={null}>
-        <group rotation={[0, 0, 0]} position={[0, 1, 0]}>
+        <group rotation={[0, Math.PI, 0]} position={[0, 0, 0]}>
           <Model />
         </group>
       </Suspense>
       <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
-      <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
+      <OrbitControls enablePan={false} enableZoom={false} />
       </XR>
     </Canvas>
-    <div id='e'>OK</div>
+
     </>
   )
 
